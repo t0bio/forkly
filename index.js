f40a1a89ccedf2c38a8980d49a81f9a152129e23
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2023-07-17 14:17:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 // index.js
 
 const express = require('express');
+const axios = require('axios'); // axios for api calls
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+app.set('view engine', 'ejs'); // set view engine to ejs
 
-app.get('/search', (req, res) => {
-    res.send('Search page');
-    }
-);
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/search', async (req, res) => {
+    const query = req.query.q;
+    const key = '02dc9c177b404c14ab292cdf12bf3418'
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${query}&number=10`;
+    const response = await axios.get(url);
+    const recipes = response.data.results;
+    res.render('search', { recipes });
+
+});
 
 app.listen(3000, () => {
-    console.log('Test listening on port 3000!');
+    console.log('Test listening on port 3000');
     }
 );
