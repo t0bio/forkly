0298c16e035104c225e978dd16941a8e87b70c28
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2023-07-18 13:46:19 -0400

    fixing index.js

--- forkly-frontend/src/App.js
+++ forkly-frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
   return (
--- index.js
+++ index.js
@@ -10,20 +10,29 @@ app.set('views', './views'); // set views directory
 app.use(express.static('forkly-frontend/build'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('views/index.ejs');
 });
 
-app.get('/search', async (req, res) => {
+const quer = document.getElementById('search');
+// recover the query string from the request
+// use the query string to make a request to the api
+// render the results from the api in the index.ejs view template
+app.get('/search', (req, res) => {
     const query = req.query.q;
     const key = '02dc9c177b404c14ab292cdf12bf3418'
-    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${query}&number=10`;
-    const response = await axios.get(url);
-    const recipes = response.data.results;
-    res.render('search', { recipes });
-
+    const url = `https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${query}&number=10`;
+    axios.get(url)
+        .then(response => {
+            const results = response.data.results;
+            res.render('index', { results });
+        })
+        .catch(err => {
+            console.log(err);
+            res.render('index', { results: [] });
+        });
 });
 
 app.listen(3000, () => {
-    console.log('Test listening on port 3000');
+    console.log('App listening on port 3000');
     }
 );
