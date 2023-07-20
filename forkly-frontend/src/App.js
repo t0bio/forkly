2dd6e88acf6ed5bb178fd063821c3de3dfb2c0bf
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2023-07-20 15:06:31 -0400

    Results.js component

--- forkly-frontend/src/App.js
+++ forkly-frontend/src/App.js
@@ -3,16 +3,17 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import Navbar from './components/Navbar';
 import Searchbar from './components/Searchbar';
+import Results from './components/Results';
 
 function App() {
-  	return (
-		<div class = "App">
-			<Navbar />
-			<div class = "center-page">
-				<Searchbar />
-			</div>
-		</div>
-	)
+      return (
+        <div class = "App">
+            <Navbar />
+            <div class = "center-page">
+                <Searchbar />
+            </div>
+        </div>
+    )
 }
 
 export default App;
---
+++ forkly-frontend/src/components/Results.js
@@ -0,0 +1,10 @@
+import Searchbar from './components/Searchbar';
+
+const searchTerm = document.getElementById('search').value;
+const searchRecipes = async (searchTerm) => {
+    const apiKey = '02dc9c177b404c14ab292cdf12bf3418';
+    const response = await axios.get(`https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&query=${searchTerm}&number=10`);
+    const data = await response.json();
+    return data.results;
+}
+
