82591d6fb243080cb4e5f35e1a79ca7f4e3933d7
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2023-07-20 15:19:38 -0400

    teeny edits

--- forkly-frontend/src/components/Results.js
+++ forkly-frontend/src/components/Results.js
@@ -1,6 +1,7 @@
-import Searchbar from './components/Searchbar';
+//import Searchbar from './components/Searchbar.js';
+import axios from 'axios';
 
-const searchTerm = document.getElementById('search').value;
+//const searchTerm = document.getElementById('search').value;
 const searchRecipes = async (searchTerm) => {
     const apiKey = '02dc9c177b404c14ab292cdf12bf3418';
     const response = await axios.get(`https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&query=${searchTerm}&number=10`);
